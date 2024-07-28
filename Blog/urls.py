be16a70aba187b0ad4d3ae755c2e7efcd7c0aeb1
shiamsharif <shiam.sharif.07@gmail.com>
2024-07-29 02:57:20 +0600

    ADD: Display posts from only specific user.

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogDetailView, BlogListView
+from .views import BlogDetailView, BlogListView, UserPostListView
 
 urlpatterns = [
     path("<int:pk>/", BlogDetailView.as_view(), name="detail"),
+    path('<str:username>/', UserPostListView.as_view(), name='user_posts'),
     path("", BlogListView.as_view(), name="home"),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -6,9 +8,20 @@ class BlogListView(ListView):
     model = Post
     template_name = "../templates/blog/post/home.html"
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 10
 
 
-class BlogDetailView(DeleteView):
+class BlogDetailView(DetailView):
     model = Post
     template_name="../templates/blog/post/detail.html"
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = '../templates/blog/user_posts.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        username = self.kwargs.get('username')
+        user = get_object_or_404(User, username=username)
+        return Post.objects.filter(author=user)
+    
