63f0e555e2b63a33ff1a9a0eb9186306a8a87e9d
shiamsharif <shiam.sharif.07@gmail.com>
2024-08-07 15:56:04 +0600

    ADD: Comment models and admin file

--- Blog/models.py
+++ Blog/models.py
@@ -53,3 +53,31 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={"pk": self.pk})
     
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+        
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+    
+    def __repr__(self):
+        return (
+            f"<Comment(id={self.id}, post_id={self.post.id}, name={self.name}, "
+            f"email={self.email}, created={self.created}, active={self.active})>"
+        )
--- Blog/views.py
+++ Blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 class BlogListView(ListView):
@@ -10,6 +11,16 @@ class BlogListView(ListView):
     template_name = "../templates/blog/post/home.html"
     context_object_name = 'posts'
     paginate_by = 2
+    
+# def post_share(request, post_id):
+#     post = get_object_or_404(Post, id=post_id, ststus=Post.Status.PUBLISHED)
+#     if request.method == 'POST':
+#         form = EmailPostForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#     else:
+#         form = EmailPostForm()
+#     request render
 
 
 class BlogDetailView(DetailView):
