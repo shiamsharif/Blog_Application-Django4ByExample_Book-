4cb3a9257363088d3baaacf33d1ea24150a77873
shiamsharif <shiam.sharif.07@gmail.com>
2024-08-07 21:42:34 +0600

    ADD: Comment_form.html

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BlogDetailView, BlogListView, UserPostListView
+from .views import BlogDetailView, BlogListView, UserPostListView, post_comment
+
 
 urlpatterns = [
     path("<int:pk>/", BlogDetailView.as_view(), name="detail"),
     path('<str:username>/', UserPostListView.as_view(), name='user_posts'),
+    path('<int:post_id>/comment/', post_comment.as_view(), name='post_comment'),
     path("", BlogListView.as_view(), name="home"),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 class BlogListView(ListView):
@@ -22,6 +23,19 @@ class BlogListView(ListView):
 #         form = EmailPostForm()
 #     request render
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
 
 class BlogDetailView(DetailView):
     model = Post
