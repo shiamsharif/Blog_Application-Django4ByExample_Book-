65f2fec69dc3837470bd99181495e548521c3713
shiamsharif <shiam.sharif.07@gmail.com>
2024-08-08 14:18:08 +0600

    UPDATE: urls.py

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import BlogDetailView, BlogListView, UserPostListView, post_comment
+from . import views
 
 
 urlpatterns = [
     path("<int:pk>/", BlogDetailView.as_view(), name="detail"),
     path('<str:username>/', UserPostListView.as_view(), name='user_posts'),
-    path('<int:post_id>/comment/', post_comment.as_view(), name='post_comment'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path("", BlogListView.as_view(), name="home"),
 ]
